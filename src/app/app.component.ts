d8a65f0707b60460ea16c281cc98daa8d0410b8e
Naresh Nirala <naresh.mehta8800@gmail.com>
2020-05-17 16:16:27 +0530

    iital comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'test-ng';
+  title = "test-ng";
+  browserLang;
+  constructor(private _langService: TranslateService) {
+    this.setDefaultLang();
+    this.languageChanged();
+  }
+  private setDefaultLang() {
+    this._langService.addLangs(["en", "de"]);
+    // this._langService.setDefaultLang("de");
+    this._langService.use("en");
+    this.browserLang = this._langService.getBrowserLang();
+    debugger;
+  }
+  changeLanguage(val) {
+    console.log(val.value);
+    this._langService.use(val.value);
+  }
+  languageChanged() {
+    this._langService.use(
+      this.browserLang.match(/de|en/) ? this.browserLang : "en"
+    );
+  }
 }
